7562d1295d4495aa76e81ea1069385cef778a749
noaheakin <noaheakin@gmail.com>
2020-11-16 11:32:50 -0800

    commit tree 7419a74be63b8b98c62088d5df6dbf6c19edaa8d

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,40 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (this.state.username == '' || this.state.password == '') {
+      alert("Please fill out both fields")
+    } else {
+      return this.props.handleLogin(this.state)
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={this.handleChange} id="username" name="username" type="text" value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={this.handleChange} id="password" name="password" type="password" value={this.state.password}/>
           </label>
         </div>
         <div>
